43362ad9ded1f491b826c800390af234b5f5eefc
Francisco Front <90176645+Cosifran@users.noreply.github.com>
2023-06-21 18:12:55 -0500

    line heith 18px

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: "2",
-  spaceBetween: 20,
-navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-  },
+  spaceBetween: 10,
+
   breakpoints: {
     992: {
       slidesPerView: 3,
