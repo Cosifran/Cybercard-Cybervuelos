c20f7150ce78138c9451f760913918ae5264c45c
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2023-07-11 17:53:02 -0500

    11/07/23 update

--- assets/js/search.js
+++ assets/js/search.js
@@ -91,3 +91,95 @@ function highlightButton(event) {
   }
 
 }
+
+
+
+
+//logica para al tocar el btn personalizado aparezca llegada y salida
+
+const btnFilterBlock = document.getElementById('btnBlockFilter')
+const salidaOne = document.getElementById('llegadaOne')
+const llegadaOne = document.getElementById('salidaOne')
+
+btnFilterBlock.addEventListener('click', function(){
+  const isBlock = salidaOne.classList.contains('d-none')
+
+  if(isBlock){
+    salidaOne.classList.remove('d-none')
+    salidaOne.classList.add('d-block')
+    llegadaOne.classList.remove('d-none')
+    llegadaOne.classList.add('d-block')
+  }else{
+    salidaOne.classList.remove('d-block')
+    salidaOne.classList.add('d-none')
+    llegadaOne.classList.remove('d-block')
+    llegadaOne.classList.add('d-none')
+  }
+})
+
+//funcion para agregar el borde de color rojo en los botones de filtro
+function buttonFilter(event){
+  // Obtener el elemento <a> más cercano en la jerarquía del DOM
+  const button = event.target.closest('.btn')
+
+   // Verificar si el botón ya tiene la clase 'active-button'
+   const isActive = button.classList.contains('botones--active-button');
+
+     // Obtener todos los botones dentro del carrusel
+  const buttons = document.querySelectorAll('.filterBtnSalida .btn');
+
+    // Eliminar la clase 'active-button' de todos los botones
+    buttons.forEach(btn => {
+      btn.classList.remove('botones--active-button');
+  });
+
+    // Agregar o elimina la clase 'active-button' al botón clicado
+    if (!isActive){
+      button.classList.add('botones--active-button');
+    }
+}
+
+//logica para al tocar el btn personalizado aparezca llegada y salida
+const btnFilterBlockEn = document.getElementById('btnBlockFilterEntrada')
+const salidaTwo = document.getElementById('llegadaTwo')
+const llegadaTwo = document.getElementById('salidaTwo')
+
+btnFilterBlockEn.addEventListener('click', function(){
+  const isBlock = salidaTwo.classList.contains('d-none')
+
+  if(isBlock){
+    salidaTwo.classList.remove('d-none')
+    salidaTwo.classList.add('d-block')
+    llegadaTwo.classList.remove('d-none')
+    llegadaTwo.classList.add('d-block')
+  }else{
+    salidaTwo.classList.remove('d-block')
+    salidaTwo.classList.add('d-none')
+    llegadaTwo.classList.remove('d-block')
+    llegadaTwo.classList.add('d-none')
+  }
+})
+
+//funcion para agregar el borde de color rojo en los botones de filtro
+function buttonFilterEntrada(event){
+  // Obtener el elemento <a> más cercano en la jerarquía del DOM
+  const button = event.target.closest('.btn')
+
+   // Verificar si el botón ya tiene la clase 'active-button'
+   const isActive = button.classList.contains('botones--active-button');
+
+     // Obtener todos los botones dentro del carrusel
+  const buttons = document.querySelectorAll('.filterBtnEntrada .btn');
+
+    // Eliminar la clase 'active-button' de todos los botones
+    buttons.forEach(btn => {
+      btn.classList.remove('botones--active-button');
+  });
+
+    // Agregar o elimina la clase 'active-button' al botón clicado
+    if (!isActive){
+      button.classList.add('botones--active-button');
+    }
+}
+
+
